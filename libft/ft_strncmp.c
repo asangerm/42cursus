f4fb4f5844b8549d3aadecdf8422f627de44597c
Arthur Sangerma <arthur.sangerma66@gmail.com>
2023-10-06 16:43:32 +0200

    Part 1 V

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: asangerm <asangerm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 17:37:51 by asangerm          #+#    #+#             */
-/*   Updated: 2023/10/03 17:50:59 by asangerm         ###   ########.fr       */
+/*   Updated: 2023/10/06 16:32:28 by asangerm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (n == 0)
+		return (0);
 	while (i < (n - 1) && s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return ((int)((unsigned char)s1[i] - (unsigned char)s2[i]));
 }
 
 /*
