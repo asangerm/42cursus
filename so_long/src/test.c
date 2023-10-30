79c13b8ebccf7fbe37059c71901ab15a9783bd65
Arthur Sangerma <arthur.sangerma66@gmail.com>
2023-10-30 21:08:26 +0100

    fdj

--- so_long/src/test.c
+++ so_long/src/test.c
@@ -6,7 +6,7 @@
 /*   By: asangerm <asangerm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 11:45:00 by asangerm          #+#    #+#             */
-/*   Updated: 2023/10/30 15:38:30 by asangerm         ###   ########.fr       */
+/*   Updated: 2023/10/30 20:15:32 by asangerm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,8 @@ void	display_map(char **map, int h, int w, void *mlx, void *win)
 	int		j;
 	int		width;
 	int		height;
-	void	*img;
+	void	*back;
+	void	*steve;
 	
 	i = 0;
 	j = 0;
@@ -40,17 +41,27 @@ void	display_map(char **map, int h, int w, void *mlx, void *win)
 		while (j < h)
 		{
 			if (map[i][j] == '1')
-				img = mlx_xpm_file_to_image(mlx, "./textures/bedrock64.xpm", &width, &height);
+			{
+				back = mlx_xpm_file_to_image(mlx, "./textures/bedrock64.xpm", &width, &height);
+				mlx_put_image_to_window(mlx, win, back, j*64, i*64);
+			}
 			else if (map[i][j] == '0')
-				img = mlx_xpm_file_to_image(mlx, "./textures/dirt64.xpm", &width, &height);
+			{
+				back = mlx_xpm_file_to_image(mlx, "./textures/dirt64.xpm", &width, &height);
+				mlx_put_image_to_window(mlx, win, back, j*64, i*64);
+			}
 			else if (map[i][j] == 'E')
-				img = mlx_xpm_file_to_image(mlx, "./textures/nether_portal64.xpm", &width, &height);
+			{
+				back = mlx_xpm_file_to_image(mlx, "./textures/nether_portal64.xpm", &width, &height);
+				mlx_put_image_to_window(mlx, win, back, j*64, i*64);
+			}
 			else if (map[i][j] == 'P')
 			{
-				img = mlx_xpm_file_to_image(mlx, "./textures/dirt64.xpm", &width, &height);
-				img = mlx_xpm_file_to_image(mlx, "./textures/steve_prof1.xpm", &width, &height);
+				back = mlx_xpm_file_to_image(mlx, "./textures/dirt64.xpm", &width, &height);
+				mlx_put_image_to_window(mlx, win, back, j*64, i*64);
+				steve = mlx_xpm_file_to_image(mlx, "./textures/steve_prof2.xpm", &width, &height);
+				mlx_put_image_to_window(mlx, win, steve, j*64, i*64);
 			}
-			mlx_put_image_to_window(mlx, win, img, j*64, i*64);
 			j++;
 		}
 		i++;
