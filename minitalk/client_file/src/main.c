39333e4c813e48a0cbc6413cec5a091a9d609a30
Arthur Sangerma <arthur.sangerma66@gmail.com>
2023-11-20 15:07:00 +0100

    strat communication

--- minitalk/client_file/src/main.c
+++ minitalk/client_file/src/main.c
@@ -6,17 +6,24 @@
 /*   By: asangerm <asangerm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 11:44:39 by asangerm          #+#    #+#             */
-/*   Updated: 2023/11/16 13:12:05 by asangerm         ###   ########.fr       */
+/*   Updated: 2023/11/20 14:22:58 by asangerm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	pid_t	pid;
 
+	if (argc != 3)
+	{
+		ft_printf("Error args\n");
+		return (0);
+	}
 	pid = getpid();
 	ft_printf("salut, je suis le client! Mon pid est : %d\n", pid);
+	kill(ft_atoi(argv[1]), SIGUSR1);
+	kill(ft_atoi(argv[1]), SIGUSR2);
 	return (0);
 }
--- minitalk/server_file/src/main.c
+++ minitalk/server_file/src/main.c
@@ -6,11 +6,16 @@
 /*   By: asangerm <asangerm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 11:09:33 by asangerm          #+#    #+#             */
-/*   Updated: 2023/11/16 13:35:39 by asangerm         ###   ########.fr       */
+/*   Updated: 2023/11/20 14:24:02 by asangerm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
+
+void	signal_f(int sig)
+{
+	ft_printf("signal delivered, the signal is: %d\n", sig);
+}
 
 int	main(void)
 {
@@ -18,6 +23,12 @@ int	main(void)
 
 	pid = getpid();
 	ft_printf("salut, je suis le serveur! Mon pid est : %d\n", pid);
+
+	signal(SIGUSR1, signal_f);
+	signal(SIGUSR2, signal_f);
+
+	while (1)
+		sleep(1);
 	return (0);
 }
 
