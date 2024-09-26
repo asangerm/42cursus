026ff28bf41df6538d7e353aa6a7d5e569cd8a26
asangerm <arthur.sangerma66@gmail.com>
2024-09-26 03:34:02 +0200

    cpp03

---
+++ cpp03/ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asangerm <asangerm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 18:01:12 by asangerm          #+#    #+#             */
+/*   Updated: 2024/09/24 18:06:15 by asangerm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap	a;
+	ClapTrap	b("Arthur");
+	ClapTrap	c(b);
+
+	a.attack("someone");
+	b.takeDamage(5);
+	b.beRepaired(3);
+	b.takeDamage(7);
+	b.beRepaired(12);
+	b.beRepaired(10);
+	b.takeDamage(10);
+	b.takeDamage(10);
+	b.beRepaired(10);
+	return 0;
+}
