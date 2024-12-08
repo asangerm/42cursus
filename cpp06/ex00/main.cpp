05e34f791dd492b0268a0c9a386025dab1c4db25
asangerm <arthur.sangerma66@gmail.com>
2024-12-09 00:54:46 +0100

    retry

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,17 +6,22 @@
 /*   By: asangerm <asangerm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 18:24:40 by asangerm          #+#    #+#             */
-/*   Updated: 2024/12/09 00:01:02 by asangerm         ###   ########.fr       */
+/*   Updated: 2024/12/09 00:54:17 by asangerm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Convert.hpp"
 
-int main(void)
+int main(int argc, char **argv)
 {
-	Convert::convert("s");
-	Convert::convert("14");
-	Convert::convert("-148.32f");
-	Convert::convert("-148.32");
-	Convert::convert("-inf");
+	if (argc != 2)
+	{
+		std::cout
+		<< "Wrong number of argument"
+		<< std::endl;
+		return (1);
+	}
+
+	Convert::convert(argv[1]);
+	return (0);
 }
